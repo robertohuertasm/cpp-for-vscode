1e6458981a5a33118f95f8603e9cec37ca482eb0
Roberto Huertas <roberto.huertas@outlook.com>
2017-07-10 00:37:29 +0200

    things will happen in the terminal

--- test.cpp
+++ test.cpp
@@ -1,17 +1,41 @@
 #include "iostream"
+using namespace std;
 
 bool whatsapp()
 {
-  std::cout << "Que pasaaaa" << std::endl << "amigo";
+  cout << "Que pasaaaa" << endl << "amigo";
   return false;
+}
+
+int numbers()
+{
+  int i;
+  int j;
+
+  cout << "Enter a number" << endl;
+  cin >> i;
+  cout << "You entered " << i << ". Enter another number." << endl;
+  cin >> j;
+
+  if (i < j)
+  {
+    cout << "The first number, " << i << ", is greater than the second:" << j;
+  }
+  else 
+  {
+    cout << "The second number, " << j << ", is greater than the first: " << i;
+  }
+  return 0;
 }
 
 int main()
 {
-  std::cout << "Hello world!" << std::endl;
+  cout << "Hello world!" << endl;
   auto j = 2; // auto is from c++11
-  std::cout << j << std::endl;
+  cout << j << endl;
   whatsapp();
+  numbers();
+  cin >> j;
   return 0;
 }
 

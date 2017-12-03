33d6c5bb78ec737de402c5edb98594841ed435ce
Roberto Huertas <roberto.huertas@outlook.com>
2017-12-03 01:08:45 +0100

    small perf test

--- test.cpp
+++ test.cpp
@@ -1,9 +1,14 @@
 #include "iostream"
+#include <iostream>
+#include <chrono>
+
 using namespace std;
+using namespace std::chrono;
 
 bool whatsapp()
 {
-  cout << "Que pasaaaa" << endl << "amigo";
+  cout << "Que pasaaaa" << endl
+       << "amigo";
   return false;
 }
 
@@ -21,21 +26,39 @@ int numbers()
   {
     cout << "The first number, " << i << ", is greater than the second:" << j;
   }
-  else 
+  else
   {
     cout << "The second number, " << j << ", is greater than the first: " << i;
   }
   return 0;
 }
 
-int main()
+long testLoop()
 {
-  cout << "Hello world!" << endl;
-  auto j = 2; // auto is from c++11
-  cout << j << endl;
-  whatsapp();
-  numbers();
-  cin >> j;
-  return 0;
+  long long number = 0;
+
+  for (long long i = 0; i < 1000000000; ++i)
+  {
+    number += 1;
+  }
+  return number;
 }
 
+int main()
+{
+  // cout << "Hello world!" << endl;
+  // auto j = 2; // auto is from c++11
+  // cout << j << endl;
+  // whatsapp();
+  // numbers();
+  // cin >> j;
+  // return 0;
+
+  high_resolution_clock::time_point t1 = high_resolution_clock::now();
+  auto a = testLoop();
+  high_resolution_clock::time_point t2 = high_resolution_clock::now();
+
+  auto duration = duration_cast<milliseconds>(t2 - t1).count();
+  cout << duration << "ms for " << a << " iterations";
+  return 0;
+}
